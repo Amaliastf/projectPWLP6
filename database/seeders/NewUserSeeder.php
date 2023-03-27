a3b7f556346e1d23f34f4775b7044bf2733843a3
Amaliastf <125347989+Amaliastf@users.noreply.github.com>
2023-03-27 20:25:04 +0700

    first commit

---
+++ database/seeders/NewUserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\support\Facades\DB;
+use Illuminate\support\Facades\Hash;
+
+
+class NewUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username'=>'admin',
+            'name' => 'Adminstrator Baru',
+            'email' =>'admin.baru@admin.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
